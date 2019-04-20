2aac0a1d874c94ea848c36f08b8970aed66a4876
Bezir <zmeid.krtz@gmail.com>
2019-04-20 16:22:29 -0500

    TaskDao

---
+++ src/main/java/com/wap/model/dao/TaskDao.java
@@ -0,0 +1,21 @@
+package com.wap.model.dao;
+
+import com.wap.model.dto.TaskDto;
+import com.wap.model.entity.Task;
+
+public class TaskDao extends BaseDao<Task, TaskDto> {
+
+    public TaskDao() {
+        super.typeTDto= TaskDto.class;
+        super.typeTEntity= Task.class;
+
+    }
+
+
+
+    public void save(TaskDto taskDto) {
+        super.save(taskDto);
+    }
+
+
+}
